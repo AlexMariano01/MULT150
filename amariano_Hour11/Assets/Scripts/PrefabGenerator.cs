082cd42e59853f6612466a74b2621f981219a794
AlexMariano01 <161530479+AlexMariano01@users.noreply.github.com>
2024-11-13 00:59:15 -0800

    Made Script Spawn 10 Lamps In Line

--- amariano_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ amariano_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public float spacing = 2.0f;
+
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(i * spacing, 0, 0);
+            Instantiate(prefab, spawnPosition, transform.rotation);
+        }
+    }
 
     void Update()
-    {if(Input.GetKeyDown(KeyCode.B))
+    {
+        if(Input.GetKeyDown(KeyCode.B))
         {
             Instantiate(prefab);
         }
