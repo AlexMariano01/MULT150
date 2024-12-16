92978121052daec1b63484dd621f33cac862ca25
AlexMariano01 <161530479+AlexMariano01@users.noreply.github.com>
2024-12-15 18:14:49 -0800

    Gauntlet runner created

---
+++ amariano_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,30 @@
+/*
+Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+
+I think the game is too easy because the slowing factor can easily be counter acted by just grabbing a speed powerup. On top of this, the number of speed up powerups are too consistent leaving a lot of room for the player to comeback from a bad move.
+
+What 2 changes would you do to make it easier or harder?
+
+I would lessen the amount of speed powerups and increase the slow down rate.
+
+List 2 things that would give it a "wow" factor.
+
+I think adding some models on the sides as well as adding a different mechanic such as upgrades instead of just speed powerups would make the game feel a lot more rewarding.
+
+What 2 parts of the game are fun?
+
+It's a simple game idea which can be fun for a lot of people. There is some replayability to it as well.
+
+What 2 parts of the game are tedious?
+
+The sliding on the floor when the player is moving can be pretty tedious at times. On top of this, the gameplay loop itself is tedious.
+
+Modify the obstacle prefab:
+
+I made the model a different shade of red as I like the red color when it reflects the lights from the powerups.
+
+Modify the powerup prefab:
+
+Added 3D start size to the powerup, as well as changed the color of the particles to a darker shade.
+
+*/
